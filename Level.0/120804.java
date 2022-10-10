8bc8d9e7e194301a715f741b9065c0e86f36fcb7
songunnie <103922744+songunnie@users.noreply.github.com>
2022-10-11 00:42:08 +0900

    두 수의 곱

---
+++ Level.0/120804.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int num1, int num2) {
+
+        return num1 * num2;
+    }
+}
+
+// 제한 사항을 추가한 코드
+class Solution {
+  public int solution(int num1, int num2) {
+    if (0<=num1 && num1>=100 && 0<=num2 && num2>=100) {
+    }
+    return num1 * num2;
+  }
+}
