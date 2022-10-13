ecb839e202e5f0cfae6e6c3f2792c901c2e84ef9
songunnie <103922744+songunnie@users.noreply.github.com>
2022-10-13 21:08:58 +0900

    피자 나눠 먹기(1)

---
+++ Level.0/120814.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int pizza = 0;
+        if(n % 7 == 0) {
+            pizza = n/7;
+        } else {
+            pizza = n/7 + 1;
+        }
+        return pizza;
+    }
+}
+
+// 한번 더 생각해보면,삼항 연산자가 가능하다.
+class Solution {
+    public int solution(int n) {
+        int pizza = (n%7 == 0) ? n/7 : n/7 + 1;
+      
+        return pizza;
+    }
+}
