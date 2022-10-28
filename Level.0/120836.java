26034d447c70a85d1892102b19f279bad869bc32
songunnie <103922744+songunnie@users.noreply.github.com>
2022-10-28 13:51:48 +0900

    순서쌍의 개수

---
+++ Level.0/120836.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(int n) {
+        int cnt = 0;
+        for(int i=1; i<=n; i++) {
+            if(n%i == 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+
+//ArrayList 사용한 풀이
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=1; i<=n; i++){
+            if(n%i==0) list.add(i);
+        }
+        answer = list.size();
+        return answer;
+    }
+}
+
+//Intstream 이용한 풀이
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        return (int) IntStream.rangeClosed(1, n).filter(i -> n % i == 0).count();
+    }
+}
