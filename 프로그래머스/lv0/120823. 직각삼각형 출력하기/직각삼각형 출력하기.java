fef7c53ee589b03c24b4faaa7394d6391821f44f
songunnie <103922744+songunnie@users.noreply.github.com>
2022-10-30 16:53:29 +0900

    Update 직각삼각형 출력하기.java

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -12,3 +12,17 @@ public class Solution {
         }
     }
 }
+
+//Better Solution
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println("*".repeat(i)); //.repeat 함수 사용!
+        }
+    }
+}
