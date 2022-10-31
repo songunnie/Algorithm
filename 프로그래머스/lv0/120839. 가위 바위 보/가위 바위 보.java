784d1264b2db25e797e137483eba0fa75ec14775
songunnie <103922744+songunnie@users.noreply.github.com>
2022-10-31 21:00:11 +0900

    Update 가위 바위 보.java

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,3 +1,28 @@
+//처음 생각했던 방법 = 훨씬 빠르다..:>
+class Solution {
+    public String solution(String rsp) {
+        char[] c = rsp.toCharArray();
+        StringBuilder answer = new StringBuilder();
+
+        for(int i=0; i < c.length; i++) {
+            switch(c[i]) {
+                case '0' :
+                    answer.append("5");
+                    break;
+                case '2' :
+                    answer.append("0");
+                    break;
+                case '5' :
+                    answer.append("2");
+                    break;
+            }
+        }
+        return answer.toString();
+
+    }
+}
+
+//.charAt() 이용
 class Solution {
     public String solution(String rsp) {
         String answer = "";
@@ -15,3 +40,19 @@ class Solution {
         return answer;
     }
 }
+
+//.equals() 이용
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        String[] arr = rsp.split("");
+        for(int i=0;i < arr.length; i++) {
+            if(arr[i].equals("2")) answer += "0";
+            else if(arr[i].equals("0")) answer += "5";
+            else answer += "2";
+        }
+
+        return answer;
+    }
+}
+
