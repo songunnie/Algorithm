83ea5eb27022f49e69c496faf8239c6d4c758b99
songunnie <103922744+songunnie@users.noreply.github.com>
2022-10-31 21:04:38 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,58 +1,20 @@
-//처음 생각했던 방법 = 훨씬 빠르다..:>
 class Solution {
     public String solution(String rsp) {
-        char[] c = rsp.toCharArray();
         StringBuilder answer = new StringBuilder();
-
-        for(int i=0; i < c.length; i++) {
+        char[] c = rsp.toCharArray();
+        for(int i=0; i<=rsp.length()-1; i++) {
             switch(c[i]) {
-                case '0' :
+                case '0':
                     answer.append("5");
                     break;
-                case '2' :
+                case '2':
                     answer.append("0");
                     break;
-                case '5' :
+                case '5':
                     answer.append("2");
                     break;
             }
         }
         return answer.toString();
-
     }
 }
-
-//.charAt() 이용
-class Solution {
-    public String solution(String rsp) {
-        String answer = "";
-        
-        for(int i=0; i<=rsp.length()-1; i++) {
-            if(rsp.charAt(i) == '2') {
-                answer+="0";
-            } else if(rsp.charAt(i) == '0') {
-                answer+="5";
-            } else if(rsp.charAt(i) == '5') {
-                answer+="2";
-            }
-        }
-        
-        return answer;
-    }
-}
-
-//.equals() 이용
-class Solution {
-    public String solution(String rsp) {
-        String answer = "";
-        String[] arr = rsp.split("");
-        for(int i=0;i < arr.length; i++) {
-            if(arr[i].equals("2")) answer += "0";
-            else if(arr[i].equals("0")) answer += "5";
-            else answer += "2";
-        }
-
-        return answer;
-    }
-}
-
