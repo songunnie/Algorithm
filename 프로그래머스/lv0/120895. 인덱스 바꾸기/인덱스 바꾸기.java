8e8efddb13bc9e35884afd727f090d20810b4d42
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-06 23:51:16 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] cArr = my_string.toCharArray();
+        
+        cArr[num1] = my_string.charAt(num2);
+        cArr[num2] = my_string.charAt(num1);
+        
+        return String.valueOf(cArr);
+    }
+}
