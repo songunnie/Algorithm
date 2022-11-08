bf52d69305dc82e09433744eddd2f205408afd66
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-08 22:01:50 +0900

    [level 0] Title: 7의 개수, Time: 0.06 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        String temp = Arrays.toString(array);
+        String str = Arrays.toString(array);
         
-        for(int i = 0; i<= temp.length()-1; i++) {
-            if(temp.charAt(i) == '7') {
+        for(int i = 0; i<= str.length()-1; i++) {
+            if(str.charAt(i) == '7') {
                 answer++;
             }
         }
