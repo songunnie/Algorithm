cc6968d7f052ea7dede0a418b35b562a8b5cb331
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-09 22:35:17 +0900

    Update 최댓값 만들기 （2）.java

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -11,3 +11,20 @@ class Solution {
         return num;
     }
 }
+
+//Arrays.sort()와 Math class를 활용한 풀이.
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+
+        Arrays.sort(numbers);
+
+        int edge1 = numbers[0]*numbers[1];
+        int edge2 = numbers[numbers.length-2]*numbers[numbers.length-1];
+
+        answer = Math.max(edge1,edge2);
+        return answer;
+    }
+}
