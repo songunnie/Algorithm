23ea4ab9b050bdb588831cf7e1c9e8c2b9184c00
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-09 22:33:24 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -2,23 +2,12 @@ class Solution {
     public int solution(int[] numbers) {
         int num = numbers[0]*numbers[1];
         for(int i=0; i<=numbers.length-1; i++) {
-            for(int j=0; j<i; j++) {
+            for(int j=i+1; j<=numbers.length-1; j++) {
                 if(num < numbers[i]*numbers[j]) {
                     num = numbers[i]*numbers[j];
                 }
             }
         }
         return num;
-    }
-}
-
-// Arrays.sort();
-import java.util.Arrays;
-
-class Solution {
-    public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-        return numbers[numbers.length-1] * numbers[numbers.length-2] < numbers[0] * numbers[1] ?
-            numbers[0] * numbers[1] : numbers[numbers.length-1] * numbers[numbers.length-2];
     }
 }
