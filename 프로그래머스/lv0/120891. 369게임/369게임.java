8e5a98a93a87c4dff3458afe740eb3cd2d1f2d2b
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-09 22:13:16 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String str = Integer.toString(order);
+        
+        for(int i=0; i<=str.length()-1; i++) {
+            if(str.charAt(i)%3 == 0 && str.charAt(i) != '0') {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
