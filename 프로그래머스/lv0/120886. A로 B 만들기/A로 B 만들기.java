7a1f263aabbd903e64377c950a29f0b974f47895
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-13 23:46:55 +0900

    Update A로 B 만들기.java

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -13,3 +13,6 @@ class Solution {
         return be.equals(af) ? 1:0;
     }
 }
+
+// char배열에 담아준 변수는 Arrays는 배열이 두개가 따로 생겨서 주소값이 당연히 달라져 equals를 사용하면 당연하게도 false가 나온다.
+// String 새로운 객체를 생성해 char타입의 배열을 String으로 만들어 준 다음 주소값 비교를 해야한다!
