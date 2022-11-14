32a02afcdcea2757513fb6c73fdac58d9448e9b3
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-14 23:24:44 +0900

    Update k의 개수.java

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -15,3 +15,20 @@ class Solution {
         return answer;
     }
 }
+
+//다른 풀이
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+
+        for (int num = i; num <= j; num++){
+            int tmp = num;
+            while (tmp != 0){
+                if (tmp % 10 == k)
+                    answer++;
+                tmp /= 10;
+            }
+        }
+        return answer;
+    }
+}
