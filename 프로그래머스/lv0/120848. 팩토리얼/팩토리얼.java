f75f81415601d0ea186abfe8029ad111b8922c36
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-15 22:03:22 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = 1;
+        for(int i=1; i<=10; i++) {
+            num *= i;
+            if(num <= n) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
