3d512222d1b81bd1fffb583b2a58fabfeaf9bd70
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-19 21:38:08 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.46 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,36 +1,36 @@
+// import java.util.*;
+// class Solution {
+//     public int[] solution(int[] emergency) {
+//         int[] asc = emergency.clone();
+//         int[] answer = new int[emergency.length];
+        
+//         Arrays.sort(asc);
+
+//         for(int i=0; i<=emergency.length-1; i++) {
+//             for(int j=0; j<=emergency.length-1; j++) {
+//                 if(asc[i]==emergency[j]) {
+//                     answer[j]=emergency.length-i;
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 class Solution {
     public int[] solution(int[] emergency) {
-        int[] asc = new int[emergency.length];
+        int[] asc = emergency;
         int[] answer = new int[emergency.length];
-        
-        for(int i=0; i<=emergency.length-1; i++) {
-            asc[i]=emergency[i];
-        }
-        
+       
         Arrays.sort(asc);
+        System.out.println(emergency[0]);
+        System.out.println(emergency[1]);
+        System.out.println(emergency[2]);
         
-        for(int i=0; i<=emergency.length-1; i++) {
-            for(int j=0; j<=emergency.length-1; j++) {
-                if(asc[i]==emergency[j]) {
-                    answer[j]=emergency.length-i;
-                }
-            }
-        }
-        return answer;
-    }
-}
-
-
-//.clone() 사용! 새로운 메모리 공간에 asc를 할당해준다.
-import java.util.*;
-class Solution {
-    public int[] solution(int[] emergency) {
-        int[] asc = emergency.clone();
-        int[] answer = new int[emergency.length];
-        
-        Arrays.sort(asc);
-
+        System.out.println(asc[0]);
+        System.out.println(asc[1]);
+        System.out.println(asc[2]);
         for(int i=0; i<=emergency.length-1; i++) {
             for(int j=0; j<=emergency.length-1; j++) {
                 if(asc[i]==emergency[j]) {
