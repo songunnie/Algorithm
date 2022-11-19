b7d10b58871683c80a7646577dc3f12a16ec7cb5
songunnie <103922744+songunnie@users.noreply.github.com>
2022-11-19 20:52:31 +0900

    Update 진료 순서 정하기.java

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -21,3 +21,23 @@ class Solution {
     }
 }
 
+
+//.clone() 사용! 새로운 메모리 공간에 asc를 할당해준다.
+import java.util.*;
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] asc = emergency.clone();
+        int[] answer = new int[emergency.length];
+        
+        Arrays.sort(asc);
+
+        for(int i=0; i<=emergency.length-1; i++) {
+            for(int j=0; j<=emergency.length-1; j++) {
+                if(asc[i]==emergency[j]) {
+                    answer[j]=emergency.length-i;
+                }
+            }
+        }
+        return answer;
+    }
+}
