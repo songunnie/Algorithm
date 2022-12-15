d8d26cc6f485dc967a3fa696798a85b05ba42113
songunnie <103922744+songunnie@users.noreply.github.com>
2022-12-15 22:47:11 +0900

    [level 0] Title: 특이한 정렬, Time: 3.64 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,20 @@
+import java.util.stream.*;
+
+class Solution {
+    public Integer[] solution(int[] numlist, int n) {
+
+        return IntStream.of(numlist).boxed().sorted((o1, o2) -> {
+            int o1Abs = Math.abs(o1 - n);
+            int o2Abs = Math.abs(o2 - n);
+
+            if (o1Abs == o2Abs) {
+                if (o1 > o2) return -1;
+                else if (o1 < o2) return 1;
+            } else {
+                return o1Abs - o2Abs;
+            }
+
+            return 0;
+        }).toArray(Integer[]::new);
+    }
+}
