cc3c9800026197a7d8ee34e804ddf07f55dcd801
songunnie <103922744+songunnie@users.noreply.github.com>
2023-01-08 21:41:14 +0900

    Update 정수 내림차순으로 배치하기.java
    StringBuilder sb = new StringBuilder(); 익숙해지기

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -20,3 +20,18 @@ class Solution {
         return answer;
     }
 }
+
+//이렇게 풀게 되는 그 날 까지!
+import java.util.*;
+
+class Solution {
+  public long solution(long n) {
+        String[] list = String.valueOf(n).split("");
+        Arrays.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (String aList : list) sb.append(aList);
+
+        return Long.parseLong(sb.reverse().toString());
+  }
+}
