9ae60e64e47cf94e1f133d3dc6456d272dd69c0c
songunnie <103922744+songunnie@users.noreply.github.com>
2023-01-11 21:35:51 +0900

    Update 두 정수 사이의 합.java

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -12,3 +12,15 @@ class Solution {
         return answer;
     }
 }
+
+//등차수열의 합 공식
+class Solution {
+
+    public long solution(int a, int b) {
+        return sumAtoB(Math.min(a, b), Math.max(b, a));
+    }
+
+    private long sumAtoB(long a, long b) {
+        return (b - a + 1) * (a + b) / 2;
+    }
+}
