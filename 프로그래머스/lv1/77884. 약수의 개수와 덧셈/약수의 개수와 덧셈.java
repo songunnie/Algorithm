6809bbde6278b5b60202ad063002b8116cf6eef5
songunnie <103922744+songunnie@users.noreply.github.com>
2023-02-15 22:43:50 +0900

    Update 약수의 개수와 덧셈.java

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -17,3 +17,23 @@ class Solution {
         return answer;
     }
 }
+
+// 제곱수를 통해 약수 개수 구하기~
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int i=left;i<=right;i++) {
+            //제곱수인 경우 약수의 개수가 홀수
+            if (i % Math.sqrt(i) == 0) {
+                answer -= i;
+            }
+            //제곱수가 아닌 경우 약수의 개수가 짝수
+            else {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
