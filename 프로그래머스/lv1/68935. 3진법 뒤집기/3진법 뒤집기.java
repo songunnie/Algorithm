94b2344d511ac9397075f112e3f3984e188f0ba3
songunnie <103922744+songunnie@users.noreply.github.com>
2023-02-24 22:36:46 +0000

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int n) {
-        String ternary = Integer.toString(n,3); //10 -> 3진수로
-        StringBuilder sb = new StringBuilder(ternary);
-        String reverse = sb.reverse().toString();
-        int decimal = Integer.parseInt(reverse, 3);
+        String ternary = Integer.toString(n,3); //10진법 -> 3진법으로
+        StringBuilder sb = new StringBuilder(ternary); //reverse() 사용을 위해
+        String reverse = sb.reverse().toString(); 
+        int decimal = Integer.parseInt(reverse, 3); //3진법을 10진법으로
         
         return decimal;
     }
