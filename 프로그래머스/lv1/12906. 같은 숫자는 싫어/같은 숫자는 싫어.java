760656b2cc3b146a6ad66574e39ea59569f25391
songunnie <103922744+songunnie@users.noreply.github.com>
2023-03-21 17:33:45 +0900

    Update 같은 숫자는 싫어.java

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -14,3 +14,24 @@ public class Solution {
         return arrList.stream().mapToInt(i->i).toArray();
     }
 }
+
+//Stream을 안썼다면
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> tempList = new ArrayList<Integer>();
+        int preNum = 10;
+        for(int num : arr) {
+            if(preNum != num)
+                tempList.add(num);
+            preNum = num;
+        }       
+        int[] answer = new int[tempList.size()];
+        for(int i=0; i<answer.length; i++) {
+            //.get(index)
+            answer[i] = tempList.get(i).intValue();
+        }
+        return answer;
+    }
+}
