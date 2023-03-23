48901dadeb4c1bc23e66f3fafe988ab518db57dc
songunnie <103922744+songunnie@users.noreply.github.com>
2023-03-23 23:39:10 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.71 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -2,10 +2,10 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         String[] arr = s.split("");
-        //'try',' ','hello',' ','world'
         int idx = 0;
+        
         for(String a : arr) {
-            idx = a.contains(" ") ? 0 : idx+1;
+            idx = a.equals(" ") ? 0 : idx+1;
             answer += idx % 2 == 0 ? a.toLowerCase() : a.toUpperCase();
         }
         return answer;
